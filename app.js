a4681524a2f0c3762b36ea5e4c45dcdd83e3747f
Marta Jankowska <martajan218@gmail.com>
2020-11-30 23:55:06 +0100

    cloudinary installed and app.use router for cloudinary

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const authRouter = require("./routes/auth");
+const upload = require("./routes/file-upload-routes");
 
 // MONGOOSE CONNECTION
 mongoose
@@ -38,6 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/auth", authRouter);
+app.use("/", upload);
 
 // ERROR HANDLING
 // catch 404 and forward to error handler
