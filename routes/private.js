8afbc0c5d9c012cf154aa76c8bb74c75c4a0cdc6
Marta Jankowska <martajan218@gmail.com>
2020-12-02 12:06:56 +0100

    edit route added

--- routes/private.js
+++ routes/private.js
@@ -24,4 +24,31 @@ router.get("/profile/:userId", withAuth, (req, res, next) => {
     });
 });
 
+router.patch("/profile/:userId/edit", withAuth, (req, res, next) => {
+  let defaultPic;
+  let { email, username, imageUrl } = req.body;
+
+  User.findById(req.params.userId)
+    .then((user) => {
+      req.body.imageUrl
+        ? (defaultPic = req.body.imageUrl)
+        : (defaultPic = user.imageUrl);
+
+      const updatedUser = { email, username, imageUrl: defaultPic };
+
+      const pr = User.update({ _id: req.params.userId }, updatedUser, {
+        new: true,
+      });
+      return pr;
+    })
+    .then(() => {
+      res.json({
+        message: `User with ${req.params.userId} is updated successfully.`,
+      });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
