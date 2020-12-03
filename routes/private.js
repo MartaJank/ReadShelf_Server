af8efaab8883bd458cdae2a09bd0cc6eab5aca9d
Marta Jankowska <martajan218@gmail.com>
2020-12-03 23:30:26 +0100

    routes for adding books to lists added

--- routes/private.js
+++ routes/private.js
@@ -12,17 +12,17 @@ const Books = require("../models/Books");
 const withAuth = require("../helpers/middleware");
 
 //PROFILE
-router.get("/profile/:userId", withAuth, (req, res, next) => {
+/* router.get("/profile/:userId", withAuth, (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => {
-      console.log("response:", req.user);
+      console.log("response:", user.email);
       //user.password = "*****";
       res.status(200).json(user);
     })
     .catch((err) => {
       res.json(err);
     });
-});
+}); */
 
 router.patch("/profile/:userId/edit", withAuth, (req, res, next) => {
   let defaultPic;
@@ -48,6 +48,96 @@ router.patch("/profile/:userId/edit", withAuth, (req, res, next) => {
     })
     .catch((err) => {
       res.json(err);
+    });
+});
+
+//BOOKS
+router.get("/searchBooks/books", (req, res, next) => {
+  Books.find()
+    .then((data) => {
+      console.log(data);
+      res.json(data).status(200);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+    });
+});
+
+router.post("/books/:userId/push/:name", withAuth, (req, res, next) => {
+  console.log("holaaaa", req.body.volumeInfo);
+  const { name } = req.params;
+  const { id } = req.body;
+  const {
+    title,
+    authors,
+    publisher,
+    publishedDate,
+    description,
+    industryIdentifiers,
+    readingModes,
+    pageCount,
+    printedPageCount,
+    dimensions,
+    printType,
+    categories,
+    averageRating,
+    ratingsCount,
+    maturityRating,
+    allowAnonLogging,
+    contentVersion,
+    panelizationSummary,
+    imageLinks,
+    language,
+    previewLink,
+    infoLink,
+    canonicalVolumeLink,
+  } = req.body.volumeInfo;
+
+  Books.create({
+    id,
+    title,
+    authors,
+    publisher,
+    publishedDate,
+    description,
+    industryIdentifiers,
+    readingModes,
+    pageCount,
+    printedPageCount,
+    dimensions,
+    printType,
+    categories,
+    averageRating,
+    ratingsCount,
+    maturityRating,
+    allowAnonLogging,
+    contentVersion,
+    panelizationSummary,
+    imageLinks,
+    language,
+    previewLink,
+    infoLink,
+    canonicalVolumeLink,
+  })
+
+    .then((data) => {
+      User.findByIdAndUpdate(
+        req.params.userId,
+        { $push: { [name]: req.body } },
+        { new: true }
+      )
+        .then((theResponse) => {
+          res.json(theResponse).status(200);
+        })
+        .catch((err) => {
+          res.json(err).status(500);
+        });
+      res.json(data).status(200);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
     });
 });
 
